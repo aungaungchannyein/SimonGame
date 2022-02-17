e876213a1ad3f86647f7ffb06282c8388a232f0e
ChanNyein AungAung <aungaung.channyein@gmail.com>
2022-02-17 20:58:53 +0630

    Initial Commit

---
+++ simon.js
@@ -0,0 +1,86 @@
+// $("#green").click(function()
+// {
+// 	console.log("hello");
+// }
+// );
+buttonColors=["red","blue","green","yellow"]
+
+
+gamePattern =[]
+userClickedPattern =[]
+level = 0
+$("body").click(function(){
+	$("h1").text("level "+level)
+	nextSequence()
+})
+
+function startOver(){
+	level = 0;
+	gamePattern =[]
+	userClickedPattern =[]
+
+}
+
+
+function nextSequence(){
+	userClickedPattern=[]
+	level+=1
+	$("h1").text("level "+level)
+	randomNumb = Math.floor(Math.random()*4)
+	// console.log(randomNumb)
+	randomChosenColor = buttonColors[randomNumb]
+	gamePattern.push(randomChosenColor) 
+	$("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+	makeSound(randomChosenColor)
+
+}
+
+$(".btn").click(function(){
+	userChosenColour= this.getAttribute("id")
+	makeSound(userChosenColour)
+	animatePress(userChosenColour)
+	userClickedPattern.push(userChosenColour)
+	console.log(userClickedPattern)
+	checkAnswer(userClickedPattern.length -1 )
+})
+
+function animatePress(currentColor){
+	$("#"+currentColor).addClass("pressed")
+
+	setTimeout(function(){
+		$("#"+currentColor).removeClass("pressed")
+	}, 100)
+}
+
+function makeSound(key){
+	var tom1 = new Audio("sounds/"+key+".mp3");
+	tom1.play();
+}
+
+function checkAnswer(currentLevel){
+
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+
+		if (gamePattern.length === userClickedPattern.length){
+
+			setTimeout(nextSequence,1000)
+		}
+		console.log("success")
+	}
+	else{
+		console.log("fail")
+		makeSound("wrong")
+		$("h1").text("Game Over, Press Anywhere to Restart")
+		$("body").addClass("game-over")
+		setTimeout(function(){
+		$("body").removeClass("game-over")
+	}, 100)
+		startOver()
+
+	}
+
+
+
+}
+
+
